93bc35122d45882c48a4acb9f0018a644a95f189
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2023-12-20 22:24:54 -0500

    interactive terminal

--- nest-graphql/src/hello-world/hello-world.resolver.ts
+++ nest-graphql/src/hello-world/hello-world.resolver.ts
@@ -5,7 +5,7 @@ export class HelloWorldResolver {
 
     @Query( () => String, { description: 'Hola Mundo es lo que retorna', name: 'hello' } )
     helloWorld(): string {
-        return 'Hola Mundo 🤙';
+        return 'Hola Mundo cambiado desde terminal';
     }
 
     @Query( () => Float, { name: 'randomNumber' } )
